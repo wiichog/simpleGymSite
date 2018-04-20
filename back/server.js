634bafbeed4f42e525e4f0a85a8b5558e29c253f
Juan Luis Garcia <jgarcia@martinexsa.com>
2018-04-20 12:46:23 -0600

    mysql connection

--- back/server.js
+++ back/server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var mysql = require('mysql')
 
 function onRequest(request,response){
 	console.log("A user made a request" + request.url);
@@ -6,6 +7,23 @@ function onRequest(request,response){
 	response.write("simpleGymSite");
 	response.end();
 }
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "simplegymapp"
+  });
+  
+  con.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
+	var sql = "SHOW TABLES;";
+  	con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Result " + result);
+  });
+  });
 
 http.createServer(onRequest).listen(8080);
 console.log("Server is running ...");
